3363a976872e425afb34b9657837cf70bd665773
NumpyGlumpy <khaypov03@mail.ru>
2023-02-11 18:08:12 +0300

    - More details to UI - New fragments

---
+++ app/src/main/java/com/example/facts/details/presentation/DetailsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.facts.details.presentation
+
+import androidx.fragment.app.Fragment
+
+class DetailsFragment:Fragment() {
+}
--- app/src/main/java/com/example/facts/main/presentation/MainActivity.kt
+++ app/src/main/java/com/example/facts/main/presentation/MainActivity.kt
@@ -1,11 +1,17 @@
-package com.example.facts
+package com.example.facts.main.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.facts.R
+import com.example.facts.numbers.presentation.NumbersFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, NumbersFragment())
+            .commit()
     }
 }
